600b7df2a9efb9e2629e61f395bbdd4ede1a2e75
Bereket Zelalem <Bereket.Zelalem@safaricom.et>
2023-06-04 15:04:12 +0300

    Organization section added

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const myApp = express();
 
 myApp.use(bodyParser.json());
 
+//______________________ MAIN APP SECTIONS _______________________________
 // Create new organization collection
 myApp.post('/Health_App', async (req, res) => {
   try {
@@ -40,8 +41,23 @@ myApp.post('/Health_App', async (req, res) => {
   }
 });
 
-// Create a new customer
-myApp.post('/Health_App/:id/customers', async (req, res) => {
+// Update organization info
+myApp.put('/Health_App/:id', async (req, res) => {
+  try {
+    const docRef = db.collection('Health_App').doc(req.params.id);
+    const updateFields = req.body;
+    // Add your validation logic here
+    await docRef.update(updateFields);
+    res.status(200).send({ message: 'Organization updated successfully' });
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
+});
+
+//________________________ PROFESSIONALS SECTION ___________________________________
+
+// Create a new professional
+myApp.post('/Health_App/:id/professionals', async (req, res) => {
   try {
     const { firstName, lastName, email, phone } = req.body;
     const validationErrors = [];
@@ -61,7 +77,7 @@ myApp.post('/Health_App/:id/customers', async (req, res) => {
       return res.status(400).send(validationErrors);
     }
 
-    const docRef = db.collection('Health_App').doc(req.params.id).collection('Customers').doc();
+    const docRef = db.collection('Health_App').doc(req.params.id).collection('professionals').doc();
     const data = {
       firstName,
       lastName,
@@ -75,52 +91,40 @@ myApp.post('/Health_App/:id/customers', async (req, res) => {
   }
 });
 
-// Update customer
-myApp.put('/Health_App/:reminderId/customers/:customerId', async (req, res) => {
+// Update professional
+myApp.put('/Health_App/:reminderId/professionals/:professional_ID', async (req, res) => {
   try {
     await db.collection('Health_App').doc(req.params.reminderId)
-      .collection('Customers').doc(req.params.customerId)
+      .collection('professionals').doc(req.params.professional_ID)
       .update(req.body);
-    res.status(200).send({ message: 'Customer updated successfully' });
+    res.status(200).send({ message: 'professional updated successfully' });
   } catch (error) {
     res.status(error.code).send(error.message);
   }
 });
 
-// Update organization info
-myApp.put('/Health_App/:id', async (req, res) => {
-  try {
-    const docRef = db.collection('Health_App').doc(req.params.id);
-    const updateFields = req.body;
-    // Add your validation logic here
-    await docRef.update(updateFields);
-    res.status(200).send({ message: 'Organization updated successfully' });
-  } catch (error) {
-    res.status(error.code).send(error.message);
-  }
-});
 
-// Fetch all customers
-myApp.get('/Health_App/:id/customers', async (req, res) => {
+// Fetch all professionals
+myApp.get('/Health_App/:id/professionals', async (req, res) => {
   try {
     const snap = await db.collection('Health_App').doc(req.params.id)
-      .collection('Customers').get();
-    const customers = snap.docs.map((doc) => doc.data());
+      .collection('professionals').get();
+    const professionals = snap.docs.map((doc) => doc.data());
     const totalCount = snap.size;
-    res.status(200).json({ totalCount, customers });
+    res.status(200).json({ totalCount, professionals });
   } catch (error) {
     res.status(error.code || 500).send(error.message);
   }
 });
 
-// Fetch single customer
-myApp.get('/Health_App/:id/customers/:customerId', async (req, res) => {
+// Fetch single professional
+myApp.get('/Health_App/:id/professionals/:professional_ID', async (req, res) => {
   try {
     const docRef = db.collection('Health_App').doc(req.params.id)
-      .collection('Customers').doc(req.params.customerId);
+      .collection('professionals').doc(req.params.professional_ID);
     const snap = await docRef.get();
     if (!snap.exists) {
-      return res.status(404).send({ message: 'Customer not found' });
+      return res.status(404).send({ message: 'professional not found' });
     }
     res.status(200).json(snap.data());
   } catch (error) {
@@ -138,21 +142,243 @@ myApp.delete('/Health_App/:id', async (req, res) => {
   }
 });
 
-// Delete customer
-myApp.delete('/Health_App/:id/customers/:customerId', async (req, res) => {
+// Delete professional
+myApp.delete('/Health_App/:id/professionals/:professional_ID', async (req, res) => {
   try {
     const docRef = db.collection('Health_App').doc(req.params.id)
-      .collection('Customers').doc(req.params.customerId);
+      .collection('professionals').doc(req.params.professional_ID);
     const snap = await docRef.get();
     if (!snap.exists) {
-      return res.status(404).send({ message: 'Customer not found' });
+      return res.status(404).send({ message: 'professional not found' });
     }
     await docRef.delete();
-    res.status(200).send({ message: 'Customer deleted successfully' });
+    res.status(200).send({ message: 'professional deleted successfully' });
   } catch (error) {
     res.status(error.code || 500).send(error.message);
   }
 });
+
+
+
+// ___________________________________This organization section______________________________________
+
+
+// Create a new organization
+myApp.post('/Health_App/:id/organizations', async (req, res) => {
+  try {
+    const { firstName, lastName, email, phone } = req.body;
+    const validationErrors = [];
+    if (!firstName) {
+      validationErrors.push('First Name is required');
+    }
+    if (!lastName) {
+      validationErrors.push('Last Name is required');
+    }
+    if (!email) {
+      validationErrors.push('Email is required');
+    }
+    if (!phone) {
+      validationErrors.push('Phone is required');
+    }
+    if (validationErrors.length > 0) {
+      return res.status(400).send(validationErrors);
+    }
+
+    const docRef = db.collection('Health_App').doc(req.params.id).collection('organizations').doc();
+    const data = {
+      firstName,
+      lastName,
+      email,
+      phone,
+    };
+    await docRef.set(data);
+    res.status(201).send();
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
+});
+
+
+
+// Update organization info
+myApp.put('/Health_App/:id', async (req, res) => {
+  try {
+    const docRef = db.collection('Health_App').doc(req.params.id);
+    const updateFields = req.body;
+    // Add your validation logic here
+    await docRef.update(updateFields);
+    res.status(200).send({ message: 'Organization updated successfully' });
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
+});
+
+// Fetch all organizations
+myApp.get('/Health_App/:id/organizations', async (req, res) => {
+  try {
+    const snap = await db.collection('Health_App').doc(req.params.id)
+      .collection('organizations').get();
+    const organizations = snap.docs.map((doc) => doc.data());
+    const totalCount = snap.size;
+    res.status(200).json({ totalCount, organizations });
+  } catch (error) {
+    res.status(error.code || 500).send(error.message);
+  }
+});
+
+// Fetch single organization
+myApp.get('/Health_App/:id/organizations/:organization_ID', async (req, res) => {
+  try {
+    const docRef = db.collection('Health_App').doc(req.params.id)
+      .collection('organizations').doc(req.params.organization_ID);
+    const snap = await docRef.get();
+    if (!snap.exists) {
+      return res.status(404).send({ message: 'organization not found' });
+    }
+    res.status(200).json(snap.data());
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
+});
+
+
+
+myApp.delete('/Health_App/:id/organizations/:organization_ID', async (req, res) => {
+  try {
+    const docRef = db.collection('Health_App').doc(req.params.id)
+      .collection('organizations').doc(req.params.organization_ID);
+    const snap = await docRef.get();
+    if (!snap.exists) {
+      return res.status(404).send({ message: 'organization not found' });
+    }
+    await docRef.delete();
+    res.status(200).send({ message: 'organization deleted successfully' });
+  } catch (error) {
+    res.status(error.code || 500).send(error.message);
+  }
+});
+
+
+
+
+
+
+//___________________________ Authentication TASKS______________________
+
+const validatePhone = (phone_number) => {
+  // Check if the phone number is in a valid format
+  if (!phone_number.match(/^\+1[0-9]{10}$/)) {
+    return false;
+  }
+
+  // The phone number is in a valid format
+  return true;
+};
+
+
+
+
+
+
+// Signup API
+myApp.post("/signup", async (req, res) => {
+  const { phone_number, password } = req.body;
+
+  if (!validatePhone(phone_number)) {
+
+    res.json({
+      status: "error",
+      message: "Invalid phone number"
+    });
+    return;
+  }
+
+  // Check if the user already exists in Firestore
+  const user = await usersCollection.doc(phone_number).get()
+  // const user=false;
+
+
+
+  // If the user doesn't exist, create a new user
+  if (!user) {
+    const otp = Math.random().toString(36).substring(4);
+
+    sendOTP(phone_number, otp);
+
+    // Save the OTP in Firestore
+    await usersCollection.doc(phone_number).set({
+      phone_number,
+      password,
+      otp
+    });
+
+    // Redirect the user to the otp verification page
+    res.redirect("/otp");
+  } else {
+    // Unsuccessful signup
+    res.json({
+      status: "error",
+      message: "User already exists"
+    });
+  }
+});
+
+
+
+
+
+
+// Login API
+myApp.post("/login", async (req, res) => {
+  const { phone_number, password } = req.body;
+
+  // Check if the user exists in Firestore
+  const user = await usersCollection.doc(phone_number).get();
+
+  if (user && user.data().password === password) {
+    // Login the user
+    res.json({
+      status: "success",
+      user: user.data()
+    });
+  } else {
+    // Unsuccessful login
+    res.json({
+      status: "error",
+      message: "Invalid credentials"
+    });
+  }
+});
+
+
+
+
+
+// OTP verification endpoint
+myApp.get("/otp", async (req, res) => {
+  const { phone_number } = req.query;
+
+  // Check if the OTP is valid
+  const user = await usersCollection.doc(phone_number).get();
+
+  if (user && user.data().otp === req.query.otp) {
+    // OTP is valid, login the user
+    res.json({
+      status: "success",
+      user: user.data()
+    });
+  } else {
+    // OTP is invalid
+    res.json({
+      status: "error",
+      message: "Invalid OTP"
+    });
+  }
+});
+
+
+
+
 
 // Start the Express app
 myApp.listen(3000, () => {
--- index.js
+++ index.js
@@ -7,14 +7,18 @@ const app = firebase.initializeApp({
   credential: firebase.credential.cert('./ServiceAccountKey.json'),
 });
 
+
 // Get the Firestore client
 const db = app.firestore();
 const myApp = express();
 myApp.use(bodyParser.json());
+// Create a new collection in Firestore called "users"
+const usersCollection = db.collection("users");
 
 
 
 // Create new organziaiton collection 
+
 myApp.post('/Health_App', async (req, res) => {
   const validationErrors = {};
 
@@ -44,10 +48,10 @@ myApp.post('/Health_App', async (req, res) => {
 
 
 // Create a new Customer
-myApp.post('/Health_App/:id/Customers', async (req, res) => {
+myApp.post('/Health_App/:id/professionals', async (req, res) => {
   try {
     const docRef = db.collection('Health_App').doc(req.params.id);
-    const notesCollection = docRef.collection('Customers');
+    const notesCollection = docRef.collection('professionals');
     const customer = req.body;
     const validationErrors = [];
     if (!customer.firstName) {
@@ -79,9 +83,9 @@ myApp.post('/Health_App/:id/Customers', async (req, res) => {
 
 
 // Update Customer
-myApp.put('/Health_App/:reminderId/Customers/:customerId', async (req, res) => {
+myApp.put('/Health_App/:reminderId/professionals/:professional_ID', async (req, res) => {
     try {
-      const docRef = db.collection('Health_App').doc(req.params.reminderId).collection('Customers').doc(req.params.customerId);
+      const docRef = db.collection('Health_App').doc(req.params.reminderId).collection('professionals').doc(req.params.professional_ID);
       docRef.update(req.body);
       res.status(200).send();
     } catch (error) {
@@ -108,25 +112,25 @@ myApp.put('/Health_App/:reminderId/Customers/:customerId', async (req, res) => {
 
 
 //Fetch All customer
-myApp.get('/Health_App/:id/customers', async (req, res) => {
+myApp.get('/Health_App/:id/professionals', async (req, res) => {
   try {
     const docRef = db.collection('Health_App').doc(req.params.id);
-    const notesCollection = docRef.collection('Customers');
-    const customers = await notesCollection.get();
+    const notesCollection = docRef.collection('professionals');
+    const professionals = await notesCollection.get();
 
     const customerList = [];
     
-    customers.forEach((doc) => {
+    professionals.forEach((doc) => {
       const data = doc.data();
       customerList.push(data);
     });
 
 
-    const totalCount = customers.size;
+    const totalCount = professionals.size;
 
     const response = {
       "totalCount": totalCount,
-      "Customers": customerList
+      "professionals": customerList
     };
 
     res.status(200).json(response);
@@ -137,11 +141,11 @@ myApp.get('/Health_App/:id/customers', async (req, res) => {
 });
 
 // Fetch Single Customer
-myApp.get('/Health_App/:id/customers/:customerId', async (req, res) => { 
+myApp.get('/Health_App/:id/professionals/:professional_ID', async (req, res) => { 
   try { 
       const docRef = db.collection('Health_App').doc(req.params.id); 
-      const notesCollection = docRef.collection('Customers'); 
-      const customerDoc = await notesCollection.doc(req.params.customerId).get();
+      const notesCollection = docRef.collection('professionals'); 
+      const customerDoc = await notesCollection.doc(req.params.professional_ID).get();
       if(!customerDoc.exists){ 
           res.status(400).send('Customer not found');
       }else{
@@ -152,6 +156,7 @@ myApp.get('/Health_App/:id/customers/:customerId', async (req, res) => {
       res.status(error.code).send(error.message); 
   } 
 });
+
 
 
 
@@ -167,15 +172,15 @@ myApp.delete('/Health_App/:id', async (req, res) => {
 });
 
 
-//Delete Customers
-myApp.delete('/Health_App/:id/customers/:customerId', async (req, res) => {
+//Delete professionals
+myApp.delete('/Health_App/:id/professionals/:professional_ID', async (req, res) => {
   try {
     const docRef = db.collection('Health_App').doc(req.params.id);
-    const customersCollection = docRef.collection('Customers');
-    const customerDoc = customersCollection.doc(req.params.customerId);
+    const professionalsCollection = docRef.collection('professionals');
+    const customerDoc = professionalsCollection.doc(req.params.professional_ID);
 
-    const customerSnapshot = await customerDoc.get();
-    if (!customerSnapshot.exists) {
+    const professionalsnapshot = await customerDoc.get();
+    if (!professionalsnapshot.exists) {
       return res.status(404).json({
         error: 'Customer not found'
       });
@@ -195,9 +200,117 @@ myApp.delete('/Health_App/:id/customers/:customerId', async (req, res) => {
 
 
 
+//login and signup and OTP
+
+// Login endpoint
+myApp.post("/login", async (req, res) => {
+  // Get the phone number and password from the request body
+  const { phone_number, password } = req.body;
+
+  // Check if the user exists in Firestore
+  const user = await usersCollection.doc(phone_number).get();
+
+  // If the user exists, check if the password is correct
+  if (user && user.data().password === password) {
+    // Login the user
+    res.json({
+      status: "success",
+      user: user.data()
+    });
+  } else {
+    // Unsuccessful login
+    res.json({
+      status: "error",
+      message: "Invalid credentials"
+    });
+  }
+});
+
+
+
+const validatePhone = (phone_number) => {
+  // Check if the phone number is in a valid format
+  // if (!phone_number.match(/^\+1[0-9]{10}$/)) {
+    // return false;
+  // }
+
+  // The phone number is in a valid format
+  return true;
+};
+
+// Signup endpoint
+myApp.post("/signup", async (req, res) => {
+  // Get the phone number and password from the request body
+  const { phone_number, password } = req.body;
+
+  // Check if the phone number is valid
+  if (!validatePhone(phone_number)) {
+    // The phone number is not valid
+    res.json({
+      status: "error",
+      message: "Invalid phone number"
+    });
+    return;
+  }
+
+  // Check if the user already exists in Firestore
+  // const user = await usersCollection.doc(phone_number).get()
+  const user=false;
+
+  // If the user doesn't exist, create a new user
+  if (!user) {
+    // Generate a random OTP
+    const otp = Math.random().toString(36).substring(7);
+
+    // Send the OTP to the user's phone number
+    // sendOTP(phone_number, otp);
+
+    // Save the OTP in Firestore
+    await usersCollection.doc(phone_number).set({
+      phone_number,
+      password,
+      otp
+    });
+
+    // Redirect the user to the otp verification page
+    res.redirect("/otp");
+  } else {
+    // Unsuccessful signup
+    res.json({
+      status: "error",
+      message: "User already exists"
+    });
+  }
+});
+
+// OTP verification endpoint
+myApp.get("/otp", async (req, res) => {
+  // Get the phone number from the query string
+  const { phone_number } = req.query;
+
+  // Check if the OTP is valid
+  const user = await usersCollection.doc(phone_number).get();
+
+  if (user && user.data().otp === req.query.otp) {
+    // OTP is valid, login the user
+    res.json({
+      status: "success",
+      user: user.data()
+    });
+  } else {
+    // OTP is invalid
+    res.json({
+      status: "error",
+      message: "Invalid OTP"
+    });
+  }
+});
+
+
+
+
+
 // Start the Express app
 myApp.listen(3000, () => {
   console.log('App listening on port 3000');
 });
-
-
